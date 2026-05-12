18a65b13b0f8a19aad5d859bb25bc05c542d8706
Carla <chirinosarredondo.carla@itismeucci.com>
2026-05-12 09:47:26 +0200

    COMMIT 2

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,7 +1,13 @@
 package com.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+   public static void main(String[] args) {
+        ContoCorrente conto = new ContoCorrente(1000); // Saldo iniziale 1000 €
+
+        Prelevatore t1 = new Prelevatore(conto, "Cliente-1", 700);
+        Prelevatore t2 = new Prelevatore(conto, "Cliente-2", 500);
+
+        t1.start();
+        t2.start();
     }
-}
+}    
